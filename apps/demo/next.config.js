b0bbe92c32dec9f2a27525ad8e668b5768e12401
do-magic <velten.seb@web.de>
2026-08-15 14:52:47 +0200

    plugin generated content

--- apps/demo/next.config.js
+++ apps/demo/next.config.js
@@ -1,9 +1,9 @@
-//@ts-check
+const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Next.js options go here
-  // See: https://nextjs.org/docs/app/api-reference/config/next-config-js
+  output: 'standalone',
+  outputFileTracingRoot: path.join(__dirname, '../..'),
 };
 
 module.exports = nextConfig;
